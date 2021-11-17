e2103b1b9f28b4e04b1c84c8e5b797578367c978
Mostafa <mostafarezaie.500@gmail.com>
2021-11-17 23:12:01 +0430

    Moidfy settings

--- csfmis/settings.py
+++ csfmis/settings.py
@@ -169,7 +169,7 @@ LOGIN_URL = "login"
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
